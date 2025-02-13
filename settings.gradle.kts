8b883b1af948e832200da6a5b9b6e587defd5895
kamiazya <yuki@kamiazya.tech>
2025-02-14 00:29:35 +0900

    chore: Update project in settings.gradle.kts and build.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,5 +7,5 @@
  * in the user manual at https://docs.gradle.org/7.2/userguide/multi_project_builds.html
  */
 
-rootProject.name = "kt-graphviz"
+rootProject.name = "kt-graphviz-root"
 include("lib")
