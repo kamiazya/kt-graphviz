6f859b135fd6c70e15c78fe2ac1448b4f813bab6
kamiazya <yuki@kamiazya.tech>
2025-03-21 22:26:35 +0900

    Add Kotest dependencies for enhanced testing support in Kotlin multiplatform project

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,3 +1,5 @@
+val kotestVersion = "5.9.0"
+
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
     kotlin("multiplatform") version "2.1.10"
@@ -22,16 +24,27 @@ kotlin {
         val commonMain by getting {
             dependencies {
                 implementation(kotlin("stdlib-common"))
+
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+                implementation("io.kotest:kotest-framework-api:$kotestVersion")
+                implementation("io.kotest:kotest-assertions-core:$kotestVersion")
+                implementation("io.kotest:kotest-property:$kotestVersion")
             }
         }
         val jvmMain by getting {
             dependencies {
                 implementation(kotlin("stdlib")) // JVM ではこれも必要
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+                implementation("io.kotest:kotest-framework-engine:$kotestVersion")
+                implementation("io.kotest:kotest-property:$kotestVersion")
             }
         }
     }
