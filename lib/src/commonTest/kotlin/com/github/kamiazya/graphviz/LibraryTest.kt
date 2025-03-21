bc6346ed9ded231d92e5310ed429b95300a4771d
kamiazya <yuki@kamiazya.tech>
2025-03-21 22:27:00 +0900

    Add basic Kotest specification for arithmetic validation in LibraryTest

--- lib/src/commonTest/kotlin/com/github/kamiazya/graphviz/LibraryTest.kt
+++ lib/src/commonTest/kotlin/com/github/kamiazya/graphviz/LibraryTest.kt
@@ -1,5 +1,14 @@
 package com.github.kamiazya.graphviz
 
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class MyCommonSpec : StringSpec({
+    "1 + 1 should be 2" {
+        (1 + 1) shouldBe 2
+    }
+})
+
 // import kotlin.test.*
 
 // class CompassTest {
